ad8d54e2efb7a37466e9aadaf47f8bfad3aa613d
Kwartler <you@example.com>
2025-06-26 12:54:25 +0200

    add a practice script

---
+++ lessons/Day4/scripts/Z_llm_practice.R
@@ -0,0 +1,24 @@
+#' Author: Ted Kwartler
+#' June 25, 2025
+#' Using an LLM explore and extract information from real airbnb reviews.
+
+
+# Possible Ideas
+# Sentiment analysis of each review
+# Extract attributes and then apply sentiment analysis to understand what drives the sentiment
+# Keep track of the neighborhoods to see if one neighborhood has better or worse sentiment
+
+# Libraries
+
+# Data
+reviews <- read.csv('https://raw.githubusercontent.com/kwartler/teaching-datasets/refs/heads/main/twoNeighborhoods_BOS_airbnb_reviews.csv')
+
+# Write a system prompt
+# Make a data packeet
+# Make a POST request
+
+# See if you can perform your analysis on a single review first to make sure it works as intended
+
+# You can write a loop to iterate through the documents and obtain all the results in another object.
+
+# End
