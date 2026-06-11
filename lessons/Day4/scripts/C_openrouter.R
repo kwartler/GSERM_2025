e9dd626c0355d52be75710268e96552de5b20562
Kwartler <you@example.com>
2026-06-11 15:49:08 +0200

    small updates with more examples d4 c script

--- lessons/Day4/scripts/C_openrouter.R
+++ lessons/Day4/scripts/C_openrouter.R
@@ -7,8 +7,17 @@ library(httr)
 library(jsonlite)
 
 # Inputs
-prompt   <- "What is the capital of Brazil?" 
-llmModel <- "google/gemini-3.1-flash-lite"
+prompt   <- "What is the capital of Brazil?"  
+#prompt   <- "How many words are in your response to this message?  Use only numbers." 
+
+# Example reading in a prompt
+#prompt <- paste(readLines('https://raw.githubusercontent.com/kwartler/GSERM_2025/refs/heads/main/lessons/Day4/scripts/C_prompt_examples/B_costar_prompt.txt'), collapse = ' ')
+
+llmModel <- "meta-llama/llama-3.2-1b-instruct"
+#'openai/gpt-4o-mini-search-preview'
+# "meta-llama/llama-3.2-1b-instruct"
+#"openai/gpt-5-chat"
+#"google/gemini-3.1-flash-lite"
 
 # Retrieve API Key securely from the environment
 openrouterKey <- Sys.getenv("OPENROUTER_API_KEY")
@@ -23,8 +32,8 @@ dataLLM <- list(model = llmModel,
                 messages = list(
                   list(role = "system", content = "You are a helpful, smart, kind, and efficient AI assistant. You always fulfill the user's requests to the best of your ability."),
                   list(role = "user", content = prompt)),
-                temperature = 0.7,
-                max_tokens = 512,
+                temperature = 2,
+                max_tokens = 512*2,
                 stream = FALSE)
 
 # Request header (OpenRouter requires Authorization, and recommends Referer/Title)
